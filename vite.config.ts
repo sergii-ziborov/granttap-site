9d52299c2477d02866b345616c5d1472acfd1802
GrantTap Publisher <publisher@granttap.com>
2026-07-24 16:58:57 +0300

    Finish GrantTap site rebrand

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: "site-creator-d1",
+          database_name: "granttap-d1",
           database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
         },
       ]
@@ -27,7 +27,7 @@ const localBindingConfig = {
     ? [
         {
           binding: r2,
-          bucket_name: "site-creator-r2",
+          bucket_name: "granttap-r2",
         },
       ]
     : [],
